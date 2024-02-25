5aa92e699833013ddc3f4c4f16f38ccd499f0ea8
JANGA GURU PAVANI <Pavani.Janga@iiitb.ac.in>
2024-02-25 18:24:37 +0530

    updated main class for proper inputs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,11 +8,7 @@ public class Main {
         Scanner sc= new Scanner(System.in);
         Calculator c = new Calculator();
 
-        System.out.println("Enter First Number :");
-        double a = sc.nextDouble();
 
-        System.out.println("Enter Second Number :");
-        double b = sc.nextDouble();
 
         System.out.println("Enter Choice :\n" +
                 "1. Addition\n" +
@@ -26,14 +22,58 @@ public class Main {
         int ch = sc.nextInt();
 
         String res = "";
+        double a,b;
+        long d;
 
         switch (ch){
-            case 1: res = c.addition(a,b);break;
-            case 2: res = c.subtraction(a,b);break;
-            case 3: res = c.multiplication(a,b);break;
-            case 4: res = c.division(a,b);break;
-//            case 5: res = c.squareRoot(a);break;
-//            case 6: res = c.factorial(Math.round(a));break;
+            case 1:
+                System.out.println("Enter First Number :");
+                a = sc.nextDouble();
+
+                System.out.println("Enter Second Number :");
+                b = sc.nextDouble();
+
+                res = c.addition(a,b);
+                break;
+            case 2:
+                System.out.println("Enter First Number :");
+                a = sc.nextDouble();
+
+                System.out.println("Enter Second Number :");
+                b = sc.nextDouble();
+
+                res = c.subtraction(a,b);
+                break;
+            case 3:
+                System.out.println("Enter First Number :");
+                a = sc.nextDouble();
+
+                System.out.println("Enter Second Number :");
+                b = sc.nextDouble();
+
+                res = c.multiplication(a,b);
+                break;
+            case 4:
+                System.out.println("Enter First Number :");
+                a = sc.nextDouble();
+
+                System.out.println("Enter Second Number :");
+                b = sc.nextDouble();
+
+                res = c.division(a,b);
+                break;
+//            case 5:
+//                System.out.println("Enter Second Number :");
+//                a = sc.nextFloat();
+//
+//                res = c.squareRoot(a);
+//                break;
+//            case 6:
+//                System.out.println("Enter Second Number :");
+//                d = sc.nextLong();
+//
+//                res = c.factorial(d);
+//                break;
             default:break;
         }
         System.out.println(res);
